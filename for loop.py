177f543f68462ca617fbf941cc81a60d2754efae
sumanth33 <53785304+sumanth33@users.noreply.github.com>
2021-05-18 07:09:46 +0530

    about for loop

---
+++ for loop.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+L = [5,2,5,2,2]
+for i in L :
+    print("x"*i)
+    
+
+
+# In[6]:
+
+
+L = [5,2,5,2,2]
+for i in L :
+    output = ""
+    for x in range(i):
+        output += "X"
+    print (output)
+
+
+# In[ ]:
+
+
+
+
